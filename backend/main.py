35e8a3ddb1bb1fc3466d4b12135635f5694f5b5e
Anderson Ortiz <ortizone620@gmail.com>
2024-10-19 17:00:23 -0600

    CORSMiddleware out

--- backend/main.py
+++ backend/main.py
@@ -1,25 +1,9 @@
 import os
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from routers import user
 
 app = FastAPI()
 
-# Configurar CORS
-origins = [
-    "http://127.0.0.1:5500",  # Tu entorno local
-    "http://localhost:5500",   # Alternativa
-    "https://AnderOrtiz.github.io"  # Reemplaza <tu-usuario> por tu usuario de GitHub
-]
-
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 # Incluye el router de usuarios
 app.include_router(user.router)
@@ -27,9 +11,3 @@ app.include_router(user.router)
 @app.get("/", tags=["Home"])
 def read_root():
     return {"Hello": "World"}
-
-# Obtener el puerto desde la variable de entorno
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))  # Render especifica el puerto en esta variable
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=port)
